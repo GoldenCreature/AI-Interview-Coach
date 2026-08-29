4e87788d164a2ce018d58c17798696cc74e1a1de
이재혁 <127727548+lyler-lee@users.noreply.github.com>
2026-08-29 14:32:39 +0900

    Origin/feat/dbPR (#42)
    * DB 메인 병합 작업
    
    메인에 올라갈 DB 관련 파일 업로드
    
    * SQLite 설정 참조 변경
    
    스크립트 및 프리펩 파일 구조를 확인하기 쉽게 변경 및 설정 파일을 생성해 SQLite 관련 라이브러리 참조 문제 해결

---
+++ Assets/LJH/LJH_scripts/Testing/DBWebcamBypassTest.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+    public class DBWebcamBypassTest : MonoBehaviour
+    {
+        [Tooltip("씬에 있는 FaceScoreDBLogger를 끌어다 넣으세요")]
+        // public FaceScoreDBLogger dbLogger;
+
+        void Start()
+        {
+        /*
+            if (dbLogger != null)
+            {
+                // 웹캠 없이 가짜 표정 데이터 강제 삽입 테스트
+                dbLogger.SaveScoreToDB(85.5f, 10.2f, 2.1f, "Good", "미소가 아주 자연스럽습니다.", 4.5f);
+                Debug.Log("✅ 가상 면접 더미 데이터 DB 삽입 완료!");
+            }
+
+        */
+        }
+    }
