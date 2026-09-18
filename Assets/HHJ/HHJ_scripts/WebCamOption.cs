b349f78fe7ebd0bad6887e550e432c7276e0b5d7
ku225125 <ku225125@gmail.com>
2026-09-18 19:44:15 +0900

    UIfix&ScriptFix-hhj
    UIfix&ScriptFix
    나머지 UI 수정 및 코드 수정

--- Assets/HHJ/HHJ_scripts/WebCamOption.cs
+++ Assets/HHJ/HHJ_scripts/WebCamOption.cs
@@ -37,10 +37,25 @@ namespace WebCamOptionUI.Scripts
 
         public void StopCamTest()
         {
-            if (webCamTexture != null && webCamTexture.isPlaying)
+            // 1. 웹캠 객체가 존재하고 작동 중인 경우에만 중지
+            if (webCamTexture != null)
             {
-                webCamTexture.Stop();
+                if (webCamTexture.isPlaying)
+                {
+                    webCamTexture.Stop();
+                }
+                webCamTexture = null;
+            }
+
+            // 2. 화면 출력용 RawImage 초기화 (있을 때만)
+            if (displayImage != null)
+            {
                 displayImage.texture = null;
+            }
+
+            // 3. 카메라 연결 여부/실행 상태와 상관없이 항상 종료 메시지 출력
+            if (statusText != null)
+            {
                 statusText.text = "카메라 테스트가 종료되었습니다.";
             }
         }
